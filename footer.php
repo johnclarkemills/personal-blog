d4a494f8901ff0f66086d6779c363d23070a253e
John Clarke Mills <johnclarkemills@mac.com>
2011-07-17 21:18:24 -0700

    removing hardcoded GA call

--- footer.php
+++ footer.php
@@ -64,11 +64,5 @@
   		<?php wp_footer(); ?>
         <div class="clearfloat"></div>
         </div>
-        <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-        </script>
-        <script type="text/javascript">
-        _uacct = "UA-1806573-1";
-        urchinTracker();
-        </script>
 	</body>
 </html>
